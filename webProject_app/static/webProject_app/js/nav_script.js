3cf10fb58912a414a8b014b640a1405650822fb0
Dvrklex <dvrklex@gmail.com>
2023-03-19 23:32:11 -0300

    Home styles, urls, views

---
+++ webProject_app/static/webProject_app/js/nav_script.js
@@ -0,0 +1,38 @@
+// Obtener todos los elementos "li" en la barra de navegación
+const navItems = document.querySelectorAll('.list');
+
+// Obtener el índice del elemento activo del localStorage
+const activeItemIndex = localStorage.getItem('activeNavItem');
+
+// Variable de estado para verificar si el elemento "active" ya se ha establecido
+let activeSet = false;
+
+// Función para manejar el comportamiento de hacer clic en un elemento "li"
+function handleClick(item, index) {
+  if (!activeSet) {
+    event.preventDefault(); // detener la acción predeterminada del enlace solo si el "active" aún no se ha establecido
+  }
+  if (!item.classList.contains('active')) {
+    navItems.forEach(item => {
+      item.classList.remove('active');
+    });
+    item.classList.add('active');
+    activeSet = true; // establecer la variable de estado como verdadera una vez que se establece el "active"
+    localStorage.setItem('activeNavItem', index); // guardar el índice del elemento activo en localStorage
+    setTimeout(() => {
+      window.location.href = item.getAttribute('data-url'); // redirigir a la URL después de que se establece el atributo "active"
+    }, 500);
+  }
+}
+
+// Agregar un controlador de eventos de clic a cada elemento "li"
+navItems.forEach((item, index) => {
+  item.addEventListener('click', (event) => {
+    handleClick(item, index);
+  });
+
+  // Restaurar el estado del elemento activo al cargar la página
+  if (activeItemIndex && index === Number(activeItemIndex)) {
+    item.classList.add('active');
+  }
+});
--- webProject_app/views.py
+++ webProject_app/views.py
@@ -3,25 +3,30 @@ from django.shortcuts import render, redirect, HttpResponse
 # Vista Home
 
 def home(request):
-    return render(request, 'webProject_app/home.html')
+    view_name= 'Home' #Esto es para que el JavaScript de la vista home.html pueda identificar la vista en la que se encuentra, y así poder cambiar el color del botón de la barra de navegación.
+    return render(request, 'webProject_app/home.html',{'view_name': view_name})
 
 # Vista Servicios
 
 def servicios(request):
-    return render(request, 'webProject_app/servicios.html')
+    view_name= 'Servicios'
+    return render(request, 'webProject_app/servicios.html',{'view_name': view_name})
 
 # Vista Tienda
 
 def tienda(request):
-    return render(request, 'webProject_app/tienda.html')
+    view_name= 'Tienda'
+    return render(request, 'webProject_app/tienda.html',{'view_name': view_name})
 
 # Vista Blog
 
 def blog(request):
-    return render(request, 'webProject_app/blog.html')
+    view_name= 'Blog'
+    return render(request, 'webProject_app/blog.html',{'view_name': view_name})
 
 # Vista Contacto
 
 def contacto(request):
-    return render(request, 'webProject_app/contacto.html')
+    view_name= 'Contacto'
+    return render(request, 'webProject_app/contacto.html',{'view_name': view_name})
 
