e5406a75cd10a5703dbbfb08a483d046e7dd3bcb
Dvrklex <dvrklex@gmail.com>
2023-04-21 01:27:34 -0300

    contacto/Api Form, styles,input

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,8 +1,30 @@
 from django import forms
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError
 
 class FormularioContacto(forms.Form):
-    nombre = forms.CharField()
-    asunto = forms.CharField()
-    email = forms.EmailField()
-    mensaje = forms.CharField(widget=forms.Textarea)
+    nombre = forms.CharField(
+        required=True,
+        label='',
+        max_length=50,
+        widget=forms.TextInput(attrs={'class':'input-box','placeholder':'Nombre '})
+        )
+    email = forms.EmailField(
+        required=True,
+        label='',
+        max_length=50,
+        validators=[EmailValidator(message='Ingrese un dirección de correo válida')],
+        widget=forms.TextInput(attrs={'class':'input-box','placeholder':'Dirección email'})
+        )
+    asunto = forms.CharField(
+        required=True,
+        label='',
+        max_length=50,
+        widget=forms.TextInput(attrs={'class':'input-box','placeholder':'Asunto'})
+        )
+    mensaje = forms.CharField(
+        required=True,
+        label='',
+        max_length=1000,
+        widget=forms.Textarea(attrs={'class':'input-box message-box','placeholder':'Escriba su mensaje aquí...'}))
    
