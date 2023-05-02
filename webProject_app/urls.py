29a93bd8ec1f2bf17ea19786feda2affac0c431c
Dvrklex <dvrklex@gmail.com>
2023-05-02 20:22:10 -0300

    tienda/App tienda

--- webProject_app/urls.py
+++ webProject_app/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='Home'),
-    path('tienda/', views.tienda, name='Tienda'),
     
 ]
 
--- webProject_app/views.py
+++ webProject_app/views.py
@@ -10,11 +10,6 @@ def home(request):
         print(post.titulo)
     return render(request, 'webProject_app/home.html',{'view_name': view_name,"latest_posts":latest_posts})
 
-# Vista Tienda
-
-def tienda(request):
-    view_name= 'Tienda'
-    return render(request, 'webProject_app/tienda.html',{'view_name': view_name})
 
 # Vista Blog
 
