d5b8ffb76913149aff24965ac95a122e7882314e
Dvrklex <dvrklex@gmail.com>
2023-05-08 19:21:40 -0300

    tienda/View tienda

--- tienda/views.py
+++ tienda/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from tienda.models import CategoriaProducto, Producto
 # Create your views here.
 # Vista Tienda
 
 def tienda(request):
     view_name= 'Tienda'
-    return render(request, 'tienda/tienda.html',{'view_name': view_name})
+    context =  {
+        "tienda":"tienda/css/tienda.css",
+        "producto":"tienda/css/producto.css",
+        "carrito":"tienda/css/tienda.css",
+        "subNav":"tienda/css/sub_nav.css"
+        
+        }
+    
+    productos = Producto.objects.all()
+    return render(request, 'tienda/tienda.html',{'view_name': view_name,'context':context,'productos':productos})
